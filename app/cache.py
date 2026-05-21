13caaaebc1210838760fc4ce5a2e0ee788458296
ALRAYYAN20 <rayyanmukadam99@gmail.com>
2026-05-21 21:01:04 +0530

    Integreted upstash redis

--- app/cache.py
+++ app/cache.py
@@ -1,3 +1,18 @@
-import redis 
+# import redis 
 
-redis_client = redis.Redis(host = 'localhost', port = 6379, db = 0, decode_responses = True )
+# redis_client = redis.Redis( host = 'localhost',
+#                             port = 6379,
+#                             db = 0,
+#                             decode_responses = True
+#                          )
+
+# refactoring to upstash redis 
+
+from upstash_redis import Redis
+from dotenv import load_dotenv 
+import os
+
+redis_client = Redis(
+    url = os.getenv("UPSTASH_REDIS_REST_URL"),
+    token = os.getenv("UPSTASH_REDIS_REST_TOKEN")
+    )
