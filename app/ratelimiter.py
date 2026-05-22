e827f34323b86b4cde9041c70faece1060a514c5
ALRAYYAN20 <rayyanmukadam99@gmail.com>
2026-05-22 23:34:50 +0530

    ratelimiter added work in progress

---
+++ app/ratelimiter.py
@@ -0,0 +1,32 @@
+from fastapi import HTTPException, Request
+from upstash_ratelimit import Ratelimit, FixedWindow
+from .cache import redis_client
+
+# ip based limiter
+ip_limiter = Ratelimit( redis = redis_client,
+                        limiter = FixedWindow ( max_request = 10, window = 60 )
+                        ) # 60 secs
+
+# user based limiter
+user_limiter = Ratelimit( redis = redis_client,
+                        limiter = FixedWindow ( max_request = 20, window = 60 )
+                        ) # 60 secs
+
+
+def check_ip_rate_limit( request: Request):
+    identifier = request.client.host
+
+    response = ip_limiter.limit(identifier)
+
+    if not response.allowed:
+        raise HTTPException ( status_code = 429, detail = " Too many requests from this IP, Try again later.")
+    
+
+def check_user_rate_limit( request: Request):
+    identifier = request.client.host
+
+    response = user_limiter.limit(identifier)
+
+    if not response.allowed:
+        raise HTTPException ( status_code = 429, detail = " Too many request from this user, Try again later.")
+    
