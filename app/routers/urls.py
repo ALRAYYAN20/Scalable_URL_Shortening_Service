14b654b4124e76b9ed67c293d415c2a749d0eba5
ALRAYYAN20 <rayyanmukadam99@gmail.com>
2026-05-23 20:50:04 +0530

    added dual layer rate limiting (ip and user based)

--- app/routers/urls.py
+++ app/routers/urls.py
@@ -8,13 +8,16 @@ from ..oauth2 import get_current_user
 from ..schemas import URLCreate , URLResponse
 from fastapi.responses import RedirectResponse
 from ..cache import redis_client
-
+from ..ratelimiter import check_user_rate_limit
 
 
 router = APIRouter()
 
+
 @router.post('/urls/')
 def url_shortener( url: URLCreate , db: Session = Depends(get_db), current_user: int = Depends(get_current_user)):
+
+    check_user_rate_limit(current_user)
 
     create_shortcode = models.URL( original_url = str(url.original_url),
                                    owner_id = current_user,
@@ -33,6 +36,8 @@ def url_shortener( url: URLCreate , db: Session = Depends(get_db), current_user:
     db.refresh(create_shortcode)
 
     return(convert_url)
+
+
 
 # user visits the short URL, gets redirected to original URL. Also increments click count.
 @router.get('/urls/{short_code}/')
@@ -71,18 +76,26 @@ def redirect_to_original_url (short_code: str, db: Session = Depends(get_db)):
 
         return RedirectResponse(url=url_entry.original_url, status_code=302)
 
+
+
 #GET /urls — get all URLs for logged in user
 @router.get('/urls/')
 def all_urls(db: Session = Depends(get_db), current_user: int = Depends(get_current_user)):
 
+    check_user_rate_limit(current_user)
+
     all_urls = db.query(models.URL).filter(models.URL.owner_id == current_user).all()
     return all_urls
+
+
 
 
 # DELETE /urls/{id} — delete a URL, check ownership first
 @router.delete('/urls/{id}')
 def by_id(id : int, db: Session = Depends(get_db), current_user: int = Depends(get_current_user)):
 
+    check_user_rate_limit(current_user)
+    
     delete_url = db.query(models.URL).filter(models.URL.id == id).first()
 
     if not delete_url:
